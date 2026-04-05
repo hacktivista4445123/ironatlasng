baa60d069b1538d0adc9b5404a635cde82794e87
hacktivista4445123 <marmoleo@proton.me>
2026-04-05 01:37:23 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
+import express from "express"
+import dotenv from "dotenv"
+import aliadosRoutes from "./routes/aliados.routes.js"
+import security from "./middlewares/security.js"
 
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+app.use(security)
+
+app.use("/api/aliados", aliadosRoutes)
+
+app.listen(process.env.PORT || 3000)
