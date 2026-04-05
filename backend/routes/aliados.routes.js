2380f87696006d2e98f43ba3368cff7a9990297f
hacktivista4445123 <marmoleo@proton.me>
2026-04-05 01:39:11 -0500

    Update aliados.routes.js

--- backend/routes/aliados.routes.js
+++ backend/routes/aliados.routes.js
@@ -1,1 +1,8 @@
+import express from "express"
+import { getAliados } from "../controllers/aliados.controller.js"
 
+const router = express.Router()
+
+router.get("/", getAliados)
+
+export default router
