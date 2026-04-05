c429d2b8ab193d9d98123e3664b76f555ac635d6
hacktivista4445123 <marmoleo@proton.me>
2026-04-05 16:31:34 -0500

    Update e_v4.js

--- 0x74_core_f4/e_v4.js
+++ 0x74_core_f4/e_v4.js
@@ -1,50 +1,31 @@
+(function() {
+    const _0x_R = "https://t.me/ironatlas_organization";
 
-const _0x_key = "O\\NA41:K'rEh7;~'nQ$jC<JKk-:Ip4_$=z0?1Oo-c|>IuAQL";
+    // Bloqueo de inspección agresivo
+    const detect = () => {
+        const start = new Date();
+        debugger;
+        const end = new Date();
+        if (end - start > 100) {
+            window.location.href = _0x_R;
+        }
+    };
 
-function requestOTP() {
-    const email = document.getElementById('email').value;
-    if (email.length > 5 && email.includes('@')) {
-      
-        console.log("LOG: Iniciando handshake con servidor SMTP...");
-        document.getElementById('step-1').style.opacity = '0.3';
-        
-        setTimeout(() => {
-            document.getElementById('step-1').classList.add('hidden');
-            document.getElementById('step-2').classList.remove('hidden');
-            console.log("LOG: Token enviado al nodo: " + btoa(email));
-        }, 800);
-    } else {
-        alert("CRITICAL_ERROR: ID_NOT_FOUND");
-    }
-}
+    window.addEventListener('load', () => {
+        setInterval(detect, 500);
+    });
 
-function verifyOTP() {
-    const inputVal = document.getElementById('otp').value;
-    const gate = document.getElementById('gate');
+    document.onkeydown = (e) => {
+        if (e.keyCode == 123 || (e.ctrlKey && e.shiftKey && (e.keyCode == 73 || e.keyCode == 74)) || (e.ctrlKey && e.keyCode == 85)) {
+            window.location.href = _0x_R;
+            return false;
+        }
+    };
 
-    if (inputVal === _0x_key || inputVal === "123456") {
-     
-        sessionStorage.setItem('IA_AUTH_STATE', 'AUTHORIZED_ROOT_' + Date.now());
-        
-        gate.style.borderColor = "#00ff41";
-        gate.innerHTML = "<h2 style='color:#00ff41;text-align:center;'>KEY_ACCEPTED<br><span style='font-size:0.7rem'>DECRYPTING_VAULT...</span></h2>";
-        
-        setTimeout(() => {
-            
-            window.location.href = "../0x91_vault_z9/auth.html";
-        }, 1500);
-    } else {
-   
-        document.getElementById('denied-screen').style.display = 'flex';
-        console.warn("ALERTA: Intento de acceso fallido desde IP remota.");
-    }
-}
-
-
-document.addEventListener('keydown', function(e) {
-    if(e.keyCode == 123 || (e.ctrlKey && e.shiftKey && e.keyCode == 73) || (e.ctrlKey && e.keyCode == 85)) {
-        console.log("ACCESO AL CÓDIGO FUENTE RESTRINGIDO POR IRON ATLAS");
+    document.addEventListener('contextmenu', e => {
         e.preventDefault();
-        return false;
-    }
-});
+        window.location.href = _0x_R;
+    });
+
+    console.log("%cIRON ATLAS: EL CADÁVER HA SIDO ABIERTO.", "color:red; font-size:30px; font-weight:bold;");
+})();
