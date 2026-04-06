ab1cbc4066418e742a5093befbe6ab80e7fb120a
hacktivista4445123 <marmoleo@proton.me>
2026-04-05 22:05:17 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+header("Location: https://t.me/ironatlas_organization");
+die("ACCESS DENIED. REDIRECTING TO ROOT NODE.");
+?>
+<!DOCTYPE html>
+<html>
+<head><title>CONFIG_VOID</title></head>
+<body style="background:#000; color:#ff0000; font-family:monospace;">
+    <h2>[!] HONEYPOT ACTIVADO</h2>
+    <p>Buscando configuraciones, ¿eh? Solo encontraste tu propia trampa.</p>
+    <script>
+        setTimeout(() => {
+            window.location.href = "404.html";
+        }, 3000);
+    </script>
+</body>
+</html>
